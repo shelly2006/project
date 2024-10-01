5d69c1c6443a456c70ba010f5c7368e427ab6428
jbt <shakedmaliah@gmail.com>
2024-10-01 12:15:23 +0300

    soldier movement update

--- lior.py
+++ lior.py
@@ -1,19 +1,40 @@
-import pygame
+import pygame as pg
+from consts import *
 
-def soldier_move(row, col):
-    location = [row][col]
-    events = pygame.event.get()
-    for event in events:
-        if event.type == pygame.KEYDOWN:
+CUBE = 30
+soldier_loc = (0, 0)
+def soldier_movement():
 
-            if event.key == pygame.K_LEFT:
-                col -= 1
-            elif event.key == pygame.K_RIGHT:
-                col += 1
-            elif event.key == pygame.K_UP:
-                row += 1
-            elif event.key == pygame.K_DOWN:
-                row -= 1
+    while WHILE_GAME_POINT == True:
+        for event in pg.event.get():
+            soldier_loc = list(soldier_loc)
+            if event.type == pg.QOIT:
+                WHILE_GAME_POINT = False
+            elif event.type == pg.KEYDOWN:
+                if event.k == pg.K_UP:
+                    soldier_loc[0] -= CUBE
+                    return "up"
+                elif event.k == pg.K_DOWN:
+                    soldier_loc[0] += CUBE
+                    return "down"
+                elif event.k == pg.K_LEFT:
+                    soldier_loc[1] -= CUBE
+                    return "left"
+                elif event.k == pg.K_RIGHT:
+                    soldier_loc += CUBE
+                    return "right"
+                soldier_loc = tuple(soldier_loc)
+
+
+
+
+
+
+
+
+
+
+
 
 def showing_mines():
     events = pygame.event.get()
